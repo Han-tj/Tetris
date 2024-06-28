484897ca697e17c1279b3fdde7b6fff34a9bf34b
Han <1053695217@qq.com>
2024-06-28 15:08:43 +0800

    check isgameover

--- GamePanel.java
+++ GamePanel.java
@@ -1,4 +1,4 @@
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -49,28 +49,60 @@ class GamePanel extends JPanel {
         }
     }
 
-    private void updateGame() {
-        if (currentTetromino.canMoveDown(board)) {
-            // 如果可以继续下移，则移动方块
-            currentTetromino.moveDown();
-        } else {
-            // 碰撞发生，固定方块并创建新的方块
-            board[currentTetromino.getY()][currentTetromino.getX()] = true;
-            currentTetromino = new Tetromino(COLS / 2, 0, Shapes.L_SHAPE);
-        }
-        if (!currentTetromino.canMoveDown(board)) {
-            // 固定当前形状到面板
-            for (int row = 0; row < currentTetromino.shape.length; row++) {
-                for (int col = 0; col < currentTetromino.shape[row].length; col++) {
-                    if (currentTetromino.shape[row][col]) {
-                        board[currentTetromino.getY() + row][currentTetromino.getX() + col] = true;
-                    }
+//    private void updateGame() {
+//        if (currentTetromino.canMoveDown(board)) {
+//            // 如果可以继续下移，则移动方块
+//            currentTetromino.moveDown();
+//        } else {
+//            // 碰撞发生，固定方块并创建新的方块
+//            board[currentTetromino.getY()][currentTetromino.getX()] = true;
+//            currentTetromino = new Tetromino(COLS / 2, 0, Shapes.L_SHAPE);
+//        }
+//        if (!currentTetromino.canMoveDown(board)) {
+//            // 固定当前形状到面板
+//            for (int row = 0; row < currentTetromino.shape.length; row++) {
+//                for (int col = 0; col < currentTetromino.shape[row].length; col++) {
+//                    if (currentTetromino.shape[row][col]) {
+//                        board[currentTetromino.getY() + row][currentTetromino.getX() + col] = true;
+//                    }
+//                }
+//            }
+//            // 创建新的L形方块
+//            currentTetromino = new Tetromino(COLS / 2, 0, Shapes.L_SHAPE);
+//        }
+//    }
+private void updateGame() {
+    // 当前方块到达底部，固定方块
+    if (currentTetromino.isAtBottom()) {
+        fixTetromino();
+    } else if (!currentTetromino.canMoveDown(board)) {
+        // 当前方块无法继续下移，固定方块
+        fixTetromino();
+    } else {
+        // 移动方块
+        currentTetromino.moveDown();
+    }
+
+    // 检查游戏是否结束
+    if (isGameOver()) {
+        // 游戏结束逻辑
+        JOptionPane.showMessageDialog(this, "游戏结束！");
+        System.exit(0); // 或者设置一个标志来停止游戏循环
+    }
+}
+
+    // 将当前的Tetromino固定到board上，并创建一个新的Tetromino
+    private void fixTetromino() {
+        for (int row = 0; row < currentTetromino.shape.length; row++) {
+            for (int col = 0; col < currentTetromino.shape[row].length; col++) {
+                if (currentTetromino.shape[row][col]) {
+                    board[currentTetromino.getY() + row][currentTetromino.getX() + col] = true;
                 }
             }
-            // 创建新的L形方块
-            currentTetromino = new Tetromino(COLS / 2, 0, Shapes.L_SHAPE);
         }
+        currentTetromino = new Tetromino(COLS / 2, 0, Shapes.L_SHAPE);
     }
+
 
     private void startGame() {
         new Thread(() -> {
@@ -84,6 +116,15 @@ class GamePanel extends JPanel {
                 }
             }
         }).start();
+    }
+
+    private boolean isGameOver() {
+        for (int col = 0; col < COLS; col++) {
+            if (board[0][col]) {
+                return true; // 如果顶行有方块，游戏结束
+            }
+        }
+        return false;
     }
 
 }
